943b51adfa714f46d3b827ee35fd838cc136d969
vitalis-virtus <vshayuk@gmail.com>
2023-03-29 14:38:05 +0300

    removed consol.logs

--- src/Components/RemindModal.js
+++ src/Components/RemindModal.js
@@ -226,13 +226,6 @@ function RemindModal({
     [deadline_at]
   );
 
-  console.log(
-    "deadline: ",
-    // deadline_at
-    transformFromStringToDate(moment.utc(deadline_at).format(noZone))
-  );
-  console.log("now: ", transformFromStringToDate(moment().format(noZone)));
-
   return (
     <AnimatePresence>
       {modalOpen && (
@@ -290,8 +283,8 @@ function RemindModal({
               {/* notification section */}
 
               {notifyStatus === "true" &&
-              deadline_at
-               > transformFromStringToDate(moment().format(noZone)) &&
+              deadline_at >
+                transformFromStringToDate(moment().format(noZone)) &&
               notifyStatus ? (
                 <div
                   className={getClasses([
